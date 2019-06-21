5dbf8dbd3e72a929d5855a6086733bac9c8e2b52
r-fujiyama <hujiyama2843@gmail.com>
2019-06-22 00:42:39 +0900

    add DMM_APIのURLを管理するenum
    ### 対応内容
    DMM_APIのURLを管理するenumを追加

---
+++ src/main/java/com/sdk/java/dmm/enums/DmmApi.java
@@ -0,0 +1,18 @@
+package com.sdk.java.dmm.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * DMM_APIを実行するためのURLを管理するenum
+ */
+@AllArgsConstructor
+@Getter
+public enum DmmAPI {
+
+  // 女優検索URL
+  ACTRESS_SEARCH("https://api.dmm.com/affiliate/v3/ActressSearch?");
+
+  private String value;
+
+}
