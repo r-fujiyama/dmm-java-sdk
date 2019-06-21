dbe65629551659797dc828925a3bf3226b4d3ff0
r-fujiyama <hujiyama2843@gmail.com>
2019-06-22 00:39:20 +0900

    add enumインタフェスを追加
    ### 対応内容
    DMM_APIの実行結果とバインドされるenumがimplementsするインタフェースを追加

---
+++ src/test/java/com/sdk/java/dmm/enums/CodeEnumTest.java
@@ -0,0 +1,47 @@
+package com.sdk.java.dmm.enums;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+
+public class CodeEnumTest {
+
+  @Test
+  public void 正常系_ofメソッド() {
+    Animal lion = Animal.of("lion");
+    assertThat(lion).isEqualTo(Animal.LION);
+    Animal zebra = Animal.of("zebra");
+    assertThat(zebra).isEqualTo(Animal.ZEBRA);
+    Animal elephant = Animal.of("elephant");
+    assertThat(elephant).isEqualTo(Animal.ELEPHANT);
+  }
+
+  @Test
+  public void 異常系_ofメソッド() {
+    Throwable exception = assertThrows(
+        IllegalArgumentException.class,
+        () -> Animal.of("none")
+    );
+    assertThat(exception.getMessage()).isEqualTo("com.sdk.java.dmm.enums.CodeEnumTest$Animal:none");
+  }
+
+  @Getter
+  @AllArgsConstructor
+  public enum Animal implements CodeEnum<String> {
+
+    LION("lion"),
+    ZEBRA("zebra"),
+    ELEPHANT("elephant");
+
+    private String value;
+
+    public static Animal of(String value) {
+      return CodeEnum.of(Animal.class, value);
+    }
+
+  }
+
+}
